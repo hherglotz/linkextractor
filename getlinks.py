03e9b72e18da643bee4c7528aade1d943265c6be
Helmut Herglotz <h.herglotz@sofasession.com>
2017-01-22 16:05:05 +0100

    Added script

---
+++ getlinks.py
@@ -0,0 +1,27 @@
+import os
+import json
+
+files = os.listdir('.')
+
+links = []
+
+for idx,file in enumerate(files):
+	print("File no: "+str(idx)+"/"+str(len(files))+".")
+	f = open(file, 'r')
+	try:
+		jsdata = json.loads(f.read())
+	except ValueError:
+		print("This file is not a JSON File")
+		continue
+	if 'url' in jsdata.viewkeys():
+		links.append(str(jsdata['url']))
+	else:
+		print("No url, going forward.")	
+	f.close()
+
+file = open('linklist.txt', 'w')
+
+for link in links:
+	file.write(str(link)+"\n")
+
+print("Done")
